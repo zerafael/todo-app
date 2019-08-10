345126085d1fdb82e386a83a7a631ca2ef8b181d
José Rafael <zerafael1@gmail.com>
2019-08-09 23:06:33 -0400

    ToDo model created

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "To-Dos"
