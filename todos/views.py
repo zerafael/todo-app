5640cc5fc072da0810ddcf800f963fddef665959
José Rafael <zerafael1@gmail.com>
2019-08-11 23:59:06 -0400

    Modified templates to display todo itens from database

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import ToDo
 
 def todo_list(request):
-    return render(request, 'todo_list.html', {})
+    todos = ToDo.objects.order_by('created_at')
+    return render(request, 'todo_list.html', {'todos': todos})
