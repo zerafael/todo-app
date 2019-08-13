d779de1501945fd446701af3053567ca1d8eaa28
José Rafael <zerafael1@gmail.com>
2019-08-13 17:54:35 -0400

    Added view and template for detailed todo item as well as CSS for the site.

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ToDo
 
 def todo_list(request):
     todos = ToDo.objects.order_by('created_at')
     return render(request, 'todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = get_object_or_404(ToDo, pk=pk)
+    return render(request, 'todo_detail.html', {'todo': todo})
