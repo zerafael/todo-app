e2f0a5f5474111d22d688bee39a94e6660001db6
José Rafael <zerafael1@gmail.com>
2019-08-13 21:38:57 -0400

    Added button to add new todo item. Add new todo page

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('todo/<int:pk>/', views.todo_detail, name='todo_detail'),
+    path('todo/new/', views.todo_new, name='todo_new'),
+    path('todo/<int:pk>/edit/', views.todo_edit, name='todo_edit')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
 from .models import ToDo
+from .forms import ToDoForm
 
 def todo_list(request):
     todos = ToDo.objects.order_by('created_at')
@@ -8,3 +10,32 @@ def todo_list(request):
 def todo_detail(request, pk):
     todo = get_object_or_404(ToDo, pk=pk)
     return render(request, 'todo_detail.html', {'todo': todo})
+
+def todo_new(request):
+
+    if request.method == "POST":
+        form = ToDoForm(request.POST)
+
+        if form.is_valid():
+            todo = form.save()
+            return redirect('todo_detail', pk=todo.pk)
+
+    else:
+        form = ToDoForm()
+
+    return render(request, 'todo_edit.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = get_object_or_404(ToDo, pk=pk)
+
+    if request.method == "POST":
+        form = ToDoForm(request.POST, instance=todo)
+
+        if form.is_valid():
+            todo = form.save()
+            return redirect('todo_detail', pk=todo.pk)
+
+    else:
+        form = ToDoForm(instance=todo)
+
+    return render(request, 'todo_edit.html', {'form': form})
